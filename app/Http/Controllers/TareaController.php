249ed1f090914bbc57a72aa352a9e62f75b88b60
unknown <fernandocasame7@gmail.com>
2020-08-19 17:58:32 -0500

    produccion

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -25,7 +25,7 @@ class TareaController extends Controller
             $verifica = DB::SELECT("SELECT * FROM respuesta WHERE tarea_idtarea = ? AND usuario_idusuario = ?",[$post->idtarea,$request->idusuario]);
             if(!empty($verifica)){
             }else{
-                $data['items'][$key] = [
+                $data[$key] = [
                     $post
                 ];
             }
